ece4ee42ba275fbb8135442bda6526f118f3f9ea
“levy-DELL” <levy.ukwi002@gmail.com>
2022-12-12 17:47:11 +0200

    Render all components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import Homepage from './components/home/homepage';
+import React, { useEffect, useState } from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from './pages/Navbar';
+import Landing from './pages/Landing';
+import DotGroupNavigation from './pages/DotGroupNavigation';
 
-const App = () => (
-  <Homepage />
-);
+const App = () => {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+  const [isTopofPage, setIsTopofPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopofPage(true);
+      if (window.scrollY !== 0) setIsTopofPage(false);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopofPage={isTopofPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+
+      <div className="">
+        {isAboveMediumScreens && (
+          <DotGroupNavigation
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
+
+      <Landing setSelectedPage={setSelectedPage} />
+    </div>
+  );
+};
 
 export default App;
